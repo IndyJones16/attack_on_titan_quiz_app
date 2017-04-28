575b7c3fbb55207210cd6e4af88edacb2d9e8d6c
Indy <indyjones209@gmail.com>
2017-04-28 22:26:12 +0000

    first commit

---
+++ app/models/model.rb
@@ -0,0 +1,33 @@
+class Quiz
+	attr_accessor :q1, :q2, :q3, :q4, :q5
+
+	$score = 0
+
+	def initialize(q1,q2,q3,q4,q5)
+		@q1 = q1
+		@q2 = q2
+		@q3 = q3
+		@q4 = q4
+		@q5 = q5
+	end
+
+	def score 
+		if @q1 == "Levi Ackerman" 
+			$score = $score + 1
+		end
+		if @q2 == "Reiner Braun"
+			$score = $score + 1
+		end
+		if @q3 == "Reiss"
+			$score = $score + 1
+		end
+		if @q4 == "Farlan Church" || "Isabel Magnolia"
+			$score = $score + 1
+		end
+		if @q5 == "Petra Ral"
+			$score = $score + 1
+		end
+		return $score
+	end
+
+end
